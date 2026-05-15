35e7cab1a03d94c3a20435f28521d7e8049f0432
anant <anantjainaj2005@gmail.com>
2026-05-15 12:41:42 +0530

    Data transformation done

--- src/exception.py
+++ src/exception.py
@@ -17,11 +17,6 @@ def error_message_detail(error, error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    '''
-    Docstring for CustomException
-
-    its a class for custom exceptions
-    '''
     def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
